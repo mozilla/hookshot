9a25425a52b3711bb7674f6731ed48fc28e74a24
Kate Hudson <k88hudson@gmail.com>
2014-04-23 13:56:29 -0400

    log reponses

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var hatchet = require('hatchet');
 
 module.exports = function(env) {
 
   var router = express.Router();
+ // var auth = require('./auth');
 
-  // Healthcheck
   router.get('/', function(req, res) {
     res.send('Webmaker Hookshoot is up and running');
+  });
+
+  // https://github.com/mozilla/badgekit-api/blob/master/docs/webhooks.md
+  router.post('/webhook', function(req, res) {
+    console.log(req.body);
+    // hatchet.send('badge_application_received', {
+    //   email: 
+    // });
   });
 
   return router;
