bb159612cc48ddd111066fd23f624bd068692ae3
Erik Christensen <christensenep@gmail.com>
2014-04-25 08:53:22 -0700

    First pass at making things work

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,6 @@
 var jwt = require('jsonwebtoken');
 var crypto = require('crypto');
 
-// FYI this code is not tested and probably doesn't work
-
 module.exports = function(env) {
 
   function sha256(body) {
@@ -10,27 +8,22 @@ module.exports = function(env) {
   }
 
   return function auth(req, res, next) {
+    token = req.headers.authorization.slice(req.headers.authorization.indexOf('"')+1, -1);
 
-     var token = req.headers.authorization.slice(token.indexOf('"') + 1, -1);
+    if (!token) {
+      return next('No authorization header');
+    }
 
-     if (!token) {
-       return next('No authorization header');
-     }
-
-     jwt.verify(token, env.get('WEBHOOK_SECRET'), function(err, decodedToken) {
+    jwt.verify(token, env.get('WEBHOOK_SECRET'), function(err, decodedToken) {
       if (err) {
         return next(err);
       }
 
-      console.log(decodedToken);
-
-      if (decodedToken.payload.body.hash !== sha256(JSON.stringify(req.body))) {
+      if (decodedToken.body.hash !== sha256(JSON.stringify(req.body))) {
         return next('Request body hash does not match token hash');
       }
 
       return next();
-
      });
-
    };
 };
--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,79 @@ var express = require('express');
 var hatchet = require('hatchet');
 
 module.exports = function(env) {
-
   var router = express.Router();
-  var auth = require('./auth');
+  var auth = require('./auth')(env);
+  var badgekit = require('../lib/badgekit-api')(env);
 
   router.get('/', function(req, res) {
     res.send('Webmaker Hookshoot is up and running');
   });
 
+  function awardHook(req, res) {
+    var data = {
+      badge: req.body.badge,
+      recipient: req.body.recipient,
+      assertionUrl: req.body.assertionUrl
+    };
+
+    hatchet.send('badge_awarded', data);
+
+    return res.send(200);
+  }
+
+  function claimHook(req, res) {
+    return res.send(200);
+  }
+
+  function reviewHook(req, res) {
+    if (!req.body.application || !req.body.application.badge || !req.body.review)
+      return res.send(400, { error: 'Improperly formatted `review` call' });
+
+    const approved = req.body.approved;
+    const application = req.body.application;
+    const review = req.body.review;
+    const badge = application.badge;
+
+    function finish(err) {
+      if (err && err.code !== 409)
+        return res.send(500, { error: err });
+
+      application.processed = new Date();
+      badgekit.updateApplication({ system: badge.system.slug, badge: badge.slug, application: application }, function (err) {
+        if (err)
+          return res.send(500, { error: err});
+
+        return res.send(200);
+      });
+    }
+
+    if (approved) {
+      var query = {
+        system: badge.system.slug,
+        badge: badge.slug,
+        email: application.learner
+      }
+
+      hatchet.send('application_approved', { application: application, review: review });
+      return badgekit.createBadgeInstance(query, finish);
+    }
+    else {
+      hatchet.send('application_denied', { application: application, review: review });
+      return finish();
+    }
+  }
+
   router.post('/webhook', auth, function(req, res) {
-
-    // Testing!
-    console.log(req.body);
-
-    // This should be some stuff that is received from the webhook
-    var data = {};
-
-    hatchet.send('badge_application_received', data);
-
+    switch (req.body.action) {
+      case 'award':
+        return awardHook(req, res);
+      case 'claim':
+        return claimHook(req, res);
+      case 'review':
+        return reviewHook(req, res);
+      default:
+        return res.send(400, 'Invalid `action` specified');
+      }
   });
 
   return router;
